13cb8909b3eb9404d5b97feebf83c610a06c9916
Matthew D'Amore <msd88@drexel.edu>
2015-09-19 21:20:38 -0400

    Began testing out how to use gumbo to parse the HTML, ultimate goal would be for me to input to class #, section, and department and have it watch the class, can feel myself getting closer

--- W8List-C/main.c
+++ W8List-C/main.c
@@ -1,53 +1,80 @@
-//
-//  main.c
-//  W8List-C
-//
-//  Created by Matthew D'Amore on 9/19/15.
-//  Copyright © 2015 Matthew Damore. All rights reserved.
-//
-
 #include <stdio.h>
-#include <tidy/tidy.h>
-#include <tidy/buffio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
+#include "gumbo.h"
 
 struct string {
-    char * ptr;
+    char *ptr;
     size_t len;
 };
 
-size_t write_callback(char *ptr, size_t size, size_t nmemb, struct string *result);
-void initString(struct string *s);
-
-size_t write_callback(char *ptr, size_t size, size_t nmemb, struct string *result){
-    
-    
-    
-    
-    return 0;
+void init_string(struct string *s) {
+    s->len = 0;
+    s->ptr = malloc(s->len+1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    s->ptr[0] = '\0';
 }
 
-int main(int argc, const char * argv[]) {
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
+{
+    size_t new_len = s->len + size*nmemb;
+    s->ptr = realloc(s->ptr, new_len+1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "realloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(s->ptr+s->len, ptr, size*nmemb);
+    s->ptr[new_len] = '\0';
+    s->len = new_len;
     
-    CURL * curl;
+    return size*nmemb;
+}
+
+int main(void)
+{
+    CURL *curl;
     CURLcode res;
-    struct string result;
     
     curl = curl_easy_init();
-    if(curl){
+    if(curl) {
+        struct string s;
+        init_string(&s);
         
-        curl_easy_setopt(curl, CURLOPT_URL, "www.example.com");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
-        
+        //Massive ass url for the classes
+        curl_easy_setopt(curl, CURLOPT_URL, "https://duapp2.drexel.edu/webtms_du/app?component=courseDetails&page=CourseList&service=direct&sp=ZH4sIAAAAAAAAADWLOw7CMBAFlyA%2BNaInF8DGSKGhBFGlQeQCS7yKguzg2BtIxYm4GnfAKOKV82beH5gEDyvSndCeejKi9iyedGUbhEZGUZC3MGyUwDiHGZZc1JYYlvkNHyhDa%2BQPBEbr9jnMOSaHu47GYjAMNpW8sK%2Bb6v8fKZQtvCDpnWOYbjcqU1kMTmhMeu7QRylV2Vrtvq1QxdGkAAAA&sp=SCI&sp=SCS&sp=S13301&sp=S140&sp=5");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
         res = curl_easy_perform(curl);
         
-        if (res != CURLE_OK) {
-            printf("Something bunked up!\n");
-            exit(1);
-        }
+        // So now that i have the website, i need to use gumbo to parse out the information i need
+        GumboOutput * output = gumbo_parse(s.ptr);
         
+        //now... what do i do lol
+        GumboVector children = output->root->v.document.children;
+//        struct GumboInternalNode * childIterator = (struct GumboInternalNode * )rootChildren.data;
+//        printf("Testing out text for the child nodes: %u \n" , output->root->v.element.tag);
+        printf("Length of root children: %u \n", children.length);
+        GumboNode * body = children.data[1];
+        
+        if (body->v.element.tag == GUMBO_TAG_BODY) {
+            puts("Successfully tore out the body of the html");
+        }
+        //Now make the children vector equal to the children of the body Element
+        children = body->v.document.children;
+        
+        printf("The number of children in the body is: %u \n", children.length);
+        
+        
+        free(s.ptr);
+        
+        /* always cleanup */
+        curl_easy_cleanup(curl);
     }
-    
     return 0;
 }
+
