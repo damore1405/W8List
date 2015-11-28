ac83491de3e4eccd13d2dc47df737b9fe0b44959
Matthew D'Amore <msd88@drexel.edu>
2015-11-27 19:12:46 -0500

    Added the ability to send SMTP mail through google so that you can alert yourself when the class is open. Commenting the daemon code out so i can see whats going on in the mail process before sending it off to a child. Reduced code-reuse with getCellData function, and renamed the main method.

--- W8List-C/W8List.c
+++ W8List-C/W8List.c
@@ -1,5 +1,5 @@
 //
-//  main.c
+//  W8List.c
 //  W8List
 //
 //  Created by Matthew D'Amore on 9/20/15.
@@ -7,63 +7,62 @@
 //
 // TODO: NEED TO EMPLEMENT ERROR CHECKING SO THAT WHEN THE WEBSITE IS DOWN, SO NOBODY GETS NOTIFIED
 
-#include "main.h"
+#include "W8List.h"
 
 
 int main(int argc, char ** argv)
 {
-    //TODO: lets go ahead and daemonize
-    pid_t pid, sid;
-    
-    /* Fork off the parent process */
-    pid = fork();
-    if (pid < 0) {
-        exit(EXIT_FAILURE);
-    }
-    /* If we got a good PID, then we can exit the parent process. */
-    if (pid > 0) {
-        puts("parent process closing");
-        exit(EXIT_SUCCESS);
-    }
-    
-    /* Change the file mode mask */
-    umask(0);
-    
-    /* Open any logs here */
-    
-    setlogmask(LOG_UPTO(LOG_NOTICE));
-    openlog("testing out logging", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-    syslog(LOG_NOTICE, "W8List has started");
-    
-    /* Create a new SID for the child process */
-    sid = setsid();
-    if (sid < 0) {
-        /* Log any failures here */
-        exit(EXIT_FAILURE);
-    }
-    
-    
-    /* Change the current working directory */
+//    //TODO: lets go ahead and daemonize
+//    pid_t pid, sid;
+//    
+//    /* Fork off the parent process */
+//    pid = fork();
+//    if (pid < 0) {
+//        exit(EXIT_FAILURE);
+//    }
+//    /* If we got a good PID, then we can exit the parent process. */
+//    if (pid > 0) {
+//        puts("parent process closing");
+//        exit(EXIT_SUCCESS);
+//    }
+//    
+//    /* Change the file mode mask */
+//    umask(0);
+//    
+//    // Set up the logger
+//    setlogmask(LOG_UPTO(LOG_NOTICE));
+//    openlog("W8List Daemon", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+//    syslog(LOG_NOTICE, "W8List has started");
+//    
+//    /* Create a new SID for the child process */
+//    sid = setsid();
+//    if (sid < 0) {
+//        /* Log any failures here */
+//        exit(EXIT_FAILURE);
+//    }
+//    
+//    
+//    /* Change the current working directory */
 //    if ((chdir("/")) < 0) {
 //        /* Log any failures here */
 //        exit(EXIT_FAILURE);
 //    }
-    
-    /* Close out the standard file descriptors */
-    puts("Child process closing");
-    close(STDIN_FILENO);
+//    
+//    /* Close out the standard file descriptors */
+//    close(STDIN_FILENO);
 //    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-    
-    /* Daemon-specific initialization goes here */
-    
-    /* The Big Loop */
-    while (1) {
-        /* Do some task here ... */
-        puts("exiting child now");
-        sleep(1); /* wait 30 seconds */
-        
-    }
+//    close(STDERR_FILENO);
+//    
+//    /* Daemon-specific initialization goes here */
+//    
+//    /* The Big Loop */
+//    syslog(LOG_NOTICE, "The class watcher has started");
+//    while (1) {
+//        /* Do some task here ... */
+//        
+//        sleep(10);
+//        
+//    }
     
 //    exit(EXIT_SUCCESS);
     
@@ -76,7 +75,7 @@ int main(int argc, char ** argv)
     if(curl) {
         struct string s;
         init_string(&s);
-        char * baseUrl = "https://duapp2.drexel.edu/webtms_du/app?component=courseDetails&page=CourseList&service=direct";
+        char * baseUrl = malloc(sizeof(char) * 100000000);
         
         //TODO: Case based on user input for which semester to select...
         
@@ -91,23 +90,24 @@ int main(int argc, char ** argv)
          *      - CRN
          *      - Course Number
         */
-        
-        
-puts(SEMESTER_TOKENS[SPRING_2016_TOKEN]); /* Debug */
-
-        
+//        puts(SEMESTER_TOKENS[WINTER_2015_TOKEN]);
+        sprintf(baseUrl, "https://duapp2.drexel.edu/webtms_du/app?component=courseDetails2&page=CourseList&service=direct&sp=%s&sp=SCI&sp=SCS&sp=S16024&sp=S613&sp=5", SEMESTER_TOKENS[FALL_2015_TOKEN]);
+        puts(baseUrl);
         //Massive ass url for the classes
-        curl_easy_setopt(curl, CURLOPT_URL,
-                         "https://duapp2.drexel.edu/webtms_du/app?component=courseDetails&page=CourseList&service=direct&sp=&sp=SCI&sp=SCS&sp=S13301&sp=S140&sp=5");
+        curl_easy_setopt(curl, CURLOPT_URL, baseUrl);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
         res = curl_easy_perform(curl);
         
+//        if (res < 0) {
+//            //The request failed...
+//            syslog(LOG_ALERT, "The reqest to drexel TMS failed... check the configuration");
+//            return 1;
+//        }
+        
         // So now that i have the website, i need to use gumbo to parse out the information i need
         GumboOutput * output = gumbo_parse(s.ptr);
-        
-        //now... what do i do lol
         GumboVector children = output->root->v.document.children;
         
         printf("Length of root children: %u \n", children.length);
@@ -116,6 +116,10 @@ puts(SEMESTER_TOKENS[SPRING_2016_TOKEN]); /* Debug */
         if (body->v.element.tag == GUMBO_TAG_BODY) {
             puts("Successfully tore out the body of the html");
         }
+        char * str = malloc(sizeof(char) * 100);
+        long int val = 0;
+        val = strtol("25", NULL, 10);
+        printf("This is what strtol returnes: %ld \n", val);
         //Now make the children vector equal to the children of the body Element
         children = body->v.document.children;
         
@@ -124,6 +128,7 @@ puts(SEMESTER_TOKENS[SPRING_2016_TOKEN]); /* Debug */
         traverseHtml(body);
         
         free(s.ptr);
+//        free(baseUrl);
         free(output);
         curl_easy_cleanup(curl);
     }
@@ -154,6 +159,7 @@ size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
     
     return size*nmemb;
 }
+
 void traverseHtml(GumboNode * root){
     if (root->parent != NULL && root->type != GUMBO_NODE_WHITESPACE && root->type == GUMBO_NODE_TEXT) {
         if (!strcasecmp(root->v.text.text, "enroll")) {
@@ -161,49 +167,44 @@ void traverseHtml(GumboNode * root){
             //MAX ENROLL 1
             //NUMBER     3
             
-//            printf("Enroll found, it is child: %zu\n", root->parent->index_within_parent);
+//            GumboNode * tableRow = root->parent->parent;
+//            GumboNode ** kiddoes = (GumboNode** )tableRow->v.element.children.data;
+//            
+//            GumboNode * maxEnroll = kiddoes[1];
+//            GumboNode * number    = kiddoes[3];
+//            
+//            GumboNode * maxEnrollText = maxEnroll->v.element.children.data[0];
+//            GumboNode * numberText    = number   ->v.element.children.data[0];
+//            
+//            puts(maxEnrollText->v.text.text);
+//            puts(numberText->v.text.text);
             
             GumboNode * tableRow = root->parent->parent;
-            GumboNode ** kiddoes = (GumboNode** )tableRow->v.element.children.data;
-            
-            GumboNode * maxEnroll = kiddoes[1];
-            GumboNode * number    = kiddoes[3];
-            
-            GumboNode * maxEnrollText = maxEnroll->v.element.children.data[0];
-            GumboNode * numberText    = number   ->v.element.children.data[0];
-            
-            puts(maxEnrollText->v.text.text);
-            puts(numberText->v.text.text);
-            
+            const char * data = getCellData(tableRow);
+            printf("enroll: %s \n", data);
             return;
         }
-        if (!strcasecmp(root->v.text.text, "max enroll")) {
-            
-            //MAX ENROLL 1
-            //NUMBER     3
-            
-//            printf("Enroll found, it is child: %zu\n", root->parent->index_within_parent);
-            
+        else if (!strcasecmp(root->v.text.text, "max enroll")) {
             GumboNode * tableRow = root->parent->parent;
-            GumboNode ** kiddoes = (GumboNode** ) tableRow->v.element.children.data;
-            
-            GumboNode * maxEnroll = kiddoes[1];
-            GumboNode * number    = kiddoes[3];
-            
-            //Magic numbers here, not much i can do about it, "0" is simply the location of the first child
-            GumboNode * maxEnrollText = maxEnroll->v.element.children.data[0];
-            GumboNode * numberText    = number   ->v.element.children.data[0];
-            
-            puts(maxEnrollText->v.text.text);
-            puts(numberText->v.text.text);
-            
+            const char * data = getCellData(tableRow);
+            printf("max enroll: %s \n", data);
             return;
         }
-    }                                                           /* vvv  Why is this needed?????????  vvv */
+    }
+                                                            /* vvv  Why is this needed?????????  vvv */
     if (root->v.element.children.length > 0 && root->v.element.children.length <= root->v.element.children.capacity) {
         for (int i = 0; i < root->v.element.children.length ; ++i) {
             traverseHtml((GumboNode *)root->v.element.children.data[i]);
         }
     }
+    
     else return;
+}
+
+const char * getCellData(GumboNode * dataRow){
+    GumboNode ** kiddoes = (GumboNode** )dataRow->v.element.children.data;
+    GumboNode * value    = kiddoes[3]->v.element.children.data[0];
+    
+    
+    return value->v.text.text;
 }
